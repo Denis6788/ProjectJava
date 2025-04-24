16d1e7c2164478ba657086d66adefa8a9c9f5f0b
Denis6788 <dk5599@rit.edu>
2025-04-24 04:42:05 -0700

    Add files via upload

---
+++ Project 1.3/RushHourConfig.java
@@ -0,0 +1,64 @@
+
+package rushhour.model;
+
+import backtracker.Configuration;
+import backtracker.Backtracker;
+
+import java.util.*;
+
+public class RushHourConfig implements Configuration<RushHour> {
+    private RushHour game;
+    private List<String> moves;
+
+    public RushHourConfig(RushHour game) {
+        this.game = new RushHour(game);  // Deep copy to avoid mutating input
+        this.moves = new ArrayList<>();
+    }
+
+    private RushHourConfig(RushHour game, List<String> moves) {
+        this.game = game;
+        this.moves = new ArrayList<>(moves);
+    }
+
+    @Override
+    public boolean isGoal() {
+        return game.isSolved();  // You should implement this logic
+    }
+
+    @Override
+    public List<Configuration<RushHour>> getSuccessors() {
+        List<Configuration<RushHour>> successors = new ArrayList<>();
+        for (String move : game.getPossibleMoves()) {
+            RushHour copy = new RushHour(game);
+            copy.applyMove(move);
+            List<String> newMoves = new ArrayList<>(moves);
+            newMoves.add(move);
+            successors.add(new RushHourConfig(copy, newMoves));
+        }
+        return successors;
+    }
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Moves: " + String.join(", ", moves) + "\n" + game.toString();
+    }
+
+
+    public List<String> getMoves() {
+        return moves;
+    }
+
+    
+    public static RushHourConfig solve(RushHour start) {
+        RushHourConfig startConfig = new RushHourConfig(start);
+        Backtracker<RushHour> backtracker = new Backtracker<>(true);
+        Optional<Configuration<RushHour>> solution = backtracker.solve(startConfig);
+
+        return solution.map(config -> (RushHourConfig) config).orElse(null);
+    }
+}
